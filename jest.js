693a8b4e44b10fd9ff3a94c3dbbde79ce158b615
Michal Ozogan <michal@socifi.com>
2018-12-01 19:05:46 +0100

    Changed: Disable some lints for unit tests

--- jest.js
+++ jest.js
@@ -9,5 +9,7 @@ module.exports = {
     rules: {
         'compat/compat': 0, // do not check for browser support
         'promise/no-callback-in-promise': 0,
+        'sonarjs/no-duplicate-string': 0, // no need for testing env
+        'promise/catch-or-return': 0, // no need for testing env
     },
 };
